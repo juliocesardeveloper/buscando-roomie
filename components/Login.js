21d9c6d32a18a6b5f65cdf0cc9088a966c44cf3c
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-09-23 20:04:14 -0500

    Auth forms v.1.0.2

--- components/Login.js
+++ components/Login.js
@@ -2,35 +2,37 @@ import style from "../styles/Login.module.scss";
 import Link from "next/link";
 
 export default function Login({ handleClose, show }) {
-  const showHideClassName = show ? style.container : style.displayNone;
+  const showHideClassName = show ? style.principalContainer : style.displayNone;
   return (
-    <article className={showHideClassName}>
-      <div>
-        <span onClick={handleClose}>X</span>
-        <h2>Iniciar sesión</h2>
-        <form action="">
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="Correo"
-            pattern="^[\w\._]{2,30}\+?[\w]{0,10}@[\w\.\-]{3,15}\.\w{2,7}$"
-            title="Usa una cuenta de correo válida"
-            required
-          />
-          <input
-            type="password"
-            id="pwd"
-            name="pwd"
-            placeholder="Contraseña"
-            required
-          />
-          <input type="submit" value="Ingresar" />
-        </form>
-        <p>
-          ¿No tienes una cuenta? <Link href="">Regístrate</Link>
-        </p>
-      </div>
-    </article>
+    <div className={showHideClassName}>
+      <article className={style.container}>
+        <div>
+          <span onClick={handleClose}>X</span>
+          <h2>Iniciar sesión</h2>
+          <form action="">
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Correo"
+              pattern="^[\w\._]{2,30}\+?[\w]{0,10}@[\w\.\-]{3,15}\.\w{2,7}$"
+              title="Usa una cuenta de correo válida"
+              required
+            />
+            <input
+              type="password"
+              id="pwd"
+              name="pwd"
+              placeholder="Contraseña"
+              required
+            />
+            <input type="submit" value="Ingresar" />
+          </form>
+          <p>
+            ¿No tienes una cuenta? <Link href="">Regístrate</Link>
+          </p>
+        </div>
+      </article>
+    </div>
   );
 }
