dab7ef64f07c0c72c705a86b330ee1faa6c56439
pitufredic <rojas.fredic6@gmail.com>
2020-09-26 16:44:20 -0500

    fixin scss modules

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,13 @@ import styles from '../styles/Components/Home.module.scss'
 import Login from '../components/Login'
 import Register from '../components/Register'
 import Header from '../components/Header'
-<<<<<<< HEAD
-=======
+import Footer from '../components/Footer'
 import Search from '../components/Search'
 import Hero from '../components/Hero'
 import Card from '../components/Card'
 
 import { useState, useEffect } from 'react'
 
-// import Footer from '../components/Footer'
->>>>>>> b779c7c48d6dbc7d456431b7d98d98e8e2502aeb
 
 export default function Home () {
 
@@ -63,8 +60,6 @@ export default function Home () {
       <Hero
         heroImage={'https://i.picsum.photos/id/943/600/500.jpg?hmac=FDXOi156vidMKBiwEiT5-oVIpP7X4dXd54S1-1xDQRM'}
       />
-<<<<<<< HEAD
-=======
       <main >
         <h1 className={styles.title}>Habitaciones disponibles</h1>
         <section className={styles.main}>
@@ -73,10 +68,7 @@ export default function Home () {
           ))}
         </section>
       </main>
-      <div className={styles.footer}>
-        <h1>Footer</h1>
-      </div>
->>>>>>> b779c7c48d6dbc7d456431b7d98d98e8e2502aeb
+      <Footer />
     </div>
   )
 }
