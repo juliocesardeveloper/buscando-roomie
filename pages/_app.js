9ea8b893a1d3783c9fec82e5f74c5a585d16e2a2
pitufredic <rojas.fredic6@gmail.com>
2020-09-26 17:32:45 -0500

    fixing scss modules

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,16 @@ import { createStore } from "redux";
 import reducer from "../reducers/index";
 
 function MyApp ({ Component, pageProps }) {
-  return (
-	<Component {...pageProps} />
-  )
-}
+<<<<<<< HEAD
+  
 
 const initialState = { user: {} };
 const store = createStore(reducer, initialState);
 
-function MyApp({ Component, pageProps }) {
+=======
+const initialState = { user: {} };
+const store = createStore(reducer, initialState);
+>>>>>>> 89d70ad1c22697e9d9fe25145731a3f4744744e3
   return (
     <Provider store={store}>
       <Component {...pageProps} />
