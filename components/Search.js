a5346b14f236600dbe85b516bbae1b3e309f6f83
Julio César Arroyave Herrera <52511013+juliocesardeveloper@users.noreply.github.com>
2020-09-29 17:06:46 -0500

    Styles on home page

--- components/Search.js
+++ components/Search.js
@@ -1,14 +1,49 @@
+// import { useMemo } from 'react'
+// import { useForm } from '../hooks/useForm'
 import styles from '../styles/Components/Search.module.scss'
+// import Router, { useRouter } from 'next/router'
 
 const Search = () => {
+  // const router = useRouter()
+  // const { q } = router.query
+  // console.log(q)
+  // const [formValues, handleInputChange] = useForm({
+  //   searchText: q
+  // })
+
+  // const { searchText } = formValues
+
+  // const getRoomsByLocation = (location = '') => {
+  //   if (location === '') {
+  //     return []
+  //   }
+  //   location = location.toLocaleLowerCase()
+  //   return room.data.filter(room => room.data.location.toLocaleLowerCase().includes(location))
+  // }
+
+  // const roomsFiltered = useMemo(() => getRoomsByLocation(q), [q])
+
+  // const handleSearch = (e) => {
+  //   e.preventDefault()
+  //   Router.push(`?q=${searchText}`)
+  // }
+
   return (
     <>
-      <form className={styles.search} action="">
+      <form className={styles.search}>
         <input
-          type="search"
+          type="text"
           placeholder='Busca una habitación'
+          // name="searchText"
+          // autoComplete="off"
+          // value={searchText}
+          // onChange={handleInputChange}
         />
-        <button className={styles.searchIcon} type="submit"><img src='./searchIcon.svg' alt=""/></button>
+        <button
+          className={styles.searchIcon}
+          type="submit">
+          <img src='./searchIcon.svg' alt=""/>
+        </button>
       </form>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -4,46 +4,83 @@ import Search from '../components/Search'
 import Hero from '../components/Hero'
 import Card from '../components/Card'
 
-import { useState, useEffect } from "react"
+// import { useForm } from '../hooks/useForm'
+// import styles from '../styles/Components/Search.module.scss'
+// import Router, { useRouter } from 'next/router'
 
-export default function Home() {
-  
+import { useState, useEffect } from 'react'
+
+export default function Home () {
   const [room, setRoom] = useState([])
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/api/rooms")
-  //     .then((response) => response.json())
-  //     .then((data) => setRoom(data));
-  // }, []);
+  useEffect(() => {
+    fetch('http://localhost:8080/api/rooms')
+      .then((response) => response.json())
+      .then((data) => setRoom(data))
+  }, [])
+
+  // const router = useRouter()
+  // const { q = '' } = router.query
+  // console.log(q)
+  // const [formValues, handleInputChange] = useForm({
+  //   searchText: q
+  // })
+
+  // const { searchText } = formValues
+
+  // const getRoomsByLocation = (location = '') => {
+  //   if (location === '') {
+  //     return []
+  //   }
+  //   location = location.toLocaleLowerCase()
+  //   return room.filter(rooms => rooms.location.toLocaleLowerCase().includes(location))
+  // }
+
+  // const roomsFiltered = useMemo(() => getRoomsByLocation(q), [q])
+
+  // const handleSearch = (e) => {
+  //   e.preventDefault()
+  //   Router.push(`?q=${searchText}`)
+  // }
 
   return (
     <>
       <Layout>
-      <Search />
+        <Search />
         <Hero
           heroImage={
-            "https://i.picsum.photos/id/943/600/500.jpg?hmac=FDXOi156vidMKBiwEiT5-oVIpP7X4dXd54S1-1xDQRM"
+            'https://i.picsum.photos/id/943/600/500.jpg?hmac=FDXOi156vidMKBiwEiT5-oVIpP7X4dXd54S1-1xDQRM'
           }
         />
         <main>
           <h1 className={styles.title}>Habitaciones disponibles</h1>
           <section className={styles.main}>
-            {room.data?.map((item) => (
-              <Card key={item.id} {...item} />
-            ))}
-            <Card
+            {
+              room.data?.map((item) => (
+                <Card key={item.id} {...item} />
+              ))
+            }
+
+            {/* {
+              roomsFiltered.map(room => (
+                <Card
+                  key={ room.id }
+                  { ...room }
+                />
+              ))
+            } */}
+            {/* <Card
               images={
                 "https://cf.bstatic.com/images/hotel/max1024x768/213/213503501.jpg"
               }
               price={"100.000"}
               location={"Medellín"}
-            />
+            /> */}
           </section>
         </main>
-
       </Layout>
     </>
-  );
+  )
 }
 
 // const mapStateToProps = ({ eventReducer }) => eventReducer
