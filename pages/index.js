6b2676d648157ce9ea15bd8000feb436ffe3ef45
JuanDev <rexejej@gmail.com>
2020-09-30 11:44:21 -0500

    coneccion a la API

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,19 @@ import Card from '../components/Card'
 import { useState, useEffect } from "react"
 
 export default function Home() {
-  
+
   const [room, setRoom] = useState([])
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/api/rooms")
-  //     .then((response) => response.json())
-  //     .then((data) => setRoom(data));
-  // }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/rooms")
+      .then((response) => response.json())
+      .then((data) => setRoom(data));
+  }, []);
 
   return (
     <>
       <Layout>
-      <Search />
+        <Search />
         <Hero
           heroImage={
             "https://i.picsum.photos/id/943/600/500.jpg?hmac=FDXOi156vidMKBiwEiT5-oVIpP7X4dXd54S1-1xDQRM"
@@ -30,14 +30,8 @@ export default function Home() {
           <section className={styles.main}>
             {room.data?.map((item) => (
               <Card key={item.id} {...item} />
-            ))}
-            <Card
-              images={
-                "https://cf.bstatic.com/images/hotel/max1024x768/213/213503501.jpg"
-              }
-              price={"100.000"}
-              location={"Medellín"}
-            />
+            ))
+            }
           </section>
         </main>
 
