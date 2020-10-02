4d327ee1b035dd0d30ebd458e42d1fbac1d492cb
juliocesardeveloper <juliocesardeveloper@gmail.com>
2020-10-01 23:15:57 -0500

    Adding filter usability

--- components/Search.js
+++ components/Search.js
@@ -1,21 +1,35 @@
-
 import styles from '../styles/Components/Search.module.scss'
+import { useFilter } from '../hooks/useFilter'
+import { useRouter } from 'next/router'
 
 const cities = ['Baja California', 'Campeche', 'CDMX', 'Chiapas', 'Cuernavaca']
 
 const Search = () => {
+  const router = useRouter()
+  const [formValues, handleInputChange] = useFilter({
+    searchText: ''
+  })
+
+  const { searchText } = formValues
+
+  const handleFilter = (e) => {
+    e.preventDefault()
+    router.push(`?q=${searchText}`)
+  }
+
   return (
     <>
-      <form className={styles.search}>
+      <form onSubmit={handleFilter} className={styles.search}>
         <label>Escoge una ciudad:</label>
-        <select className={styles.options} id="rooms" name="rooms">
+        <select className={styles.options} id="cities" name="searchText" value={searchText} onChange={handleInputChange}>
           {cities.map((item, index) => (
             <option key={index}>{ item }</option>
           ))}
         </select>
         <button
           className={styles.searchIcon}
-          type="submit">
+          type="submit"
+        >
           <img src='./searchIcon.svg' alt=""/>
         </button>
       </form>
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import Layout from '../components/Layout'
 import Search from '../components/Search'
 import Hero from '../components/Hero'
 import Card from '../components/Card'
-// import axios from 'axios'
-// import { useState, useEffect } from 'react'
+
 import { useRouter } from 'next/router'
 import useFetchCardRoom from '../hooks/useFetchCardRoom'
 
@@ -31,22 +30,9 @@ export default function Home () {
                 <Card key={item.id} {...item} />
               ))
             }
-
-            {/* <Card
-              images={
-                "https://cf.bstatic.com/images/hotel/max1024x768/213/213503501.jpg"
-              }
-              price={"100.000"}
-              location={"Medellín"}
-            /> */}
           </section>
         </main>
       </Layout>
     </>
   )
 }
-
-// const mapStateToProps = ({ eventReducer }) => eventReducer
-// const mapDispatchToProps = {
-//   getEventRecents
-// }
