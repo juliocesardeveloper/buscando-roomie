4484c9023cafee58b1472943e27d5157f6b15474
santiago.arcilag <santiago.arcilag@gmail.com>
2020-10-03 01:17:31 -0500

    solving more conflicts

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-import "../styles/globals.scss";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducer from "../reducers/index";
-import Layout from "../components/Layout";
-=======
 import '../styles/globals.scss'
->>>>>>> frontend
 
 function MyApp ({ Component, pageProps }) {
   return (
-<<<<<<< HEAD
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
-  );
-=======
     <Component {...pageProps} />
   )
->>>>>>> frontend
 }
 export default MyApp
